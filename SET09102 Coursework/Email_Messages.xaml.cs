1b978a25ca6687b4fa4089e22f90d263f8f7ce52
40397559 <40397559@live.napier.ac.uk>
2020-11-01 15:10:32 +0000

    Created blank forms for all types of messages

---
+++ SET09102 Coursework/Email_Messages.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SET09102_Coursework
+{
+    /// <summary>
+    /// Interaction logic for Email_Messages.xaml
+    /// </summary>
+    public partial class Email_Messages : Window
+    {
+        public Email_Messages()
+        {
+            InitializeComponent();
+        }
+    }
+}
